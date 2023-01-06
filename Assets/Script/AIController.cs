5e8ad081d2f14ca9bee1b8e378054c410067fcfc
Owen Richards <or1g20@soton.ac.uk>
2023-01-06 14:40:34 +0000

    basics of ai companion

---
+++ Assets/Script/AIController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = FindClosestEnemy();
+        AIDestinationSetter aids = GetComponent<AIDestinationSetter>();
+        if (go != null){
+            aids.target = go.transform;
+        }
+    }
+
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+
+}
